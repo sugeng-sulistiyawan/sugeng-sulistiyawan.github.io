52eeaed5326eae14ed21459fdb8cc4d07c0e34f5
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2023-04-28 13:15:25 +0700

    add highlight.js module

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,23 +1,32 @@
 import hljs from 'highlight.js/lib/core';
 
+import bash from 'highlight.js/lib/languages/bash';
+import css from 'highlight.js/lib/languages/css';
+import ini from 'highlight.js/lib/languages/ini';
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
-import bash from 'highlight.js/lib/languages/bash';
-import xml from 'highlight.js/lib/languages/xml';
-import ini from 'highlight.js/lib/languages/ini';
-import yaml from 'highlight.js/lib/languages/yaml';
 import markdown from 'highlight.js/lib/languages/markdown';
-import python from 'highlight.js/lib/languages/python';
+import php from 'highlight.js/lib/languages/php';
+import scss from 'highlight.js/lib/languages/scss';
+import shell from 'highlight.js/lib/languages/shell';
+import xml from 'highlight.js/lib/languages/xml';
+import yaml from 'highlight.js/lib/languages/yaml';
 
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
-hljs.registerLanguage('html', xml);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('js', javascript);
+hljs.registerLanguage('json', json);
 hljs.registerLanguage('ini', ini);
 hljs.registerLanguage('toml', ini);
-hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('md', markdown);
-hljs.registerLanguage('python', python);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('html', xml);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('yml', yaml);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
